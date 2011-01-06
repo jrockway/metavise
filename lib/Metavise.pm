b2c7ce10cd8ae51661c00a4a368dbd3ae01e131b
Jonathan Rockway <jon@jrock.us>
2011-01-06 11:31:13 -0600

    move config to file; add "title"

--- lib/Metavise.pm
+++ lib/Metavise.pm
@@ -5,7 +5,7 @@ use true;
 use namespace::autoclean;
 
 use Catalyst qw/
-    -Debug
+    -Debug ConfigLoader
 /;
 
 extends 'Catalyst';
@@ -15,8 +15,4 @@ our $VERSION = '0.01';
 __PACKAGE__->config(
     name                                        => 'Metavise',
     disable_component_resolution_regex_fallback => 1,
-    'Model::Processes' => {
-        directory => '/home/jon/.dotfiles/services/',
-        rrd       => '/tmp/rrd/',
-    },
 );
